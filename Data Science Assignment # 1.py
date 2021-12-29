1f49d0f00dd72bebd44cc5d3828b1e960de0f6ac
muaazasif <muhammed.muaaz@gmail.com>
2021-12-30 00:29:27 +0500

    Data Science Assignment # 1

---
+++ Data Science Assignment # 1.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Twinkle, twinkle, little star,")
+print("      How I wonder what you are!")
+print("            Up above the world so high,")
+print("            Like a diamond in the sky.")
+print("Twinkle, twinkle, little star,")
+print("      How I wonder what you are!")
+
+
+# In[2]:
+
+
+import sys
+print("Python version")
+print (sys.version)
+print("Version info.")
+print (sys.version_info)
+
+
+# In[3]:
+
+
+from datetime import date
+today = date.today()
+print("Today's date:", today)
+
+
+# In[4]:
+
+
+from math import pi
+r = float(input ("Input the radius of the circle : "))
+print ("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
+
+
+# In[5]:
+
+
+fname = input("Input your First Name : ")
+lname = input("Input your Last Name : ")
+print ("Hello  " + lname + " " + fname)
+
+
+# In[6]:
+
+
+num1 = int(input("Enter First Number: "))
+num2 = int(input("Enter Second Number: "))
+a = num1 + num2
+print("Sum :", a)
+
+
+# In[ ]:
+
+
+
+
